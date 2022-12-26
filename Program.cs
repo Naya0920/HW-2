43291e3fb17156ef738e15274781c219375250a8
Naya <maa071002@gmail.com>
2022-12-26 21:44:05 +0600

    First task

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача№1
+/*
+Console.Write("Enter a three-digit number: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("Second digit of this number "+stringNumber[1]);
+*/
+// Задача№2 
+/*
+Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberText = Convert.ToString(number);
+if (numberText.Length > 2)
+{
+    Console.WriteLine("third digit " + numberText[2]);
+}
+else 
+{
+    Console.WriteLine("no third digit");
+}
+*/
